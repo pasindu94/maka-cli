8fe0cd6e358206cfb08a0f8d9307bfed8694f6de
FBRTMaka <mjmcee@gmail.com>
2016-09-28 10:21:31 -0400

    fixes ts error message on TemplateStatic class, using bracket notation instead of dot

--- lib/templates/template/template.js.ts
+++ lib/templates/template/template.js.ts
@@ -3,23 +3,23 @@ import { Template } from 'meteor/templating';
 /*****************************************************************************/
 /* <%= name %>: Event Handlers */
 /*****************************************************************************/
-Template.<%= name %>.events({
+Template['<%= name %>'].events({
 });
 
 /*****************************************************************************/
 /* <%= name %>: Helpers */
 /*****************************************************************************/
-Template.<%= name %>.helpers({
+Template['<%= name %>'].helpers({
 });
 
 /*****************************************************************************/
 /* <%= name %>: Lifecycle Hooks */
 /*****************************************************************************/
-Template.<%= name %>.onCreated(function() {
+Template['<%= name %>'].onCreated(function() {
 });
 
-Template.<%= name %>.onRendered(function() {
+Template['<%= name %>'].onRendered(function() {
 });
 
-Template.<%= name %>.onDestroyed(function() {
+Template['<%= name %>'].onDestroyed(function() {
 });
